dcc3976f3f5473c0142bb26c6b09ab5a6bec5c90
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-30 15:53:25 -0800

    initial commit

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name,
+  count(students.id) AS student_count
+FROM cohorts
+  JOIN students ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+HAVING count(students.id) >= 18
+ORDER BY count(students.id);
