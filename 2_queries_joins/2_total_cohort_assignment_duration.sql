166be68933ee8031d04e4d7ea3d094de721cf282
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-30 14:38:38 -0800

    reference cohorts.name rather than ID for better clarity

--- 2_queries_joins/2_total_cohort_assignment_duration.sql
+++ 2_queries_joins/2_total_cohort_assignment_duration.sql
@@ -2,4 +2,4 @@ SELECT SUM(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohort_id = 1;
+WHERE cohorts.name = 'FEB12';
