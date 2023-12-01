18eef74c336210a86b2f93711cffb68e798bd8db
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-30 16:17:10 -0800

    refactor to use students table and join assignment_submissions table

--- 3_queries_group-by/5_average_completion_time.sql
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -1,7 +1,7 @@
 SELECT students.name AS student,
   AVG(assignment_submissions.duration) AS average_assignment_duration
-FROM assignment_submissions
-  JOIN students ON student_id = students.id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-ORDER BY AVG(assignment_submissions.duration) DESC;
+FROM students
+  JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
