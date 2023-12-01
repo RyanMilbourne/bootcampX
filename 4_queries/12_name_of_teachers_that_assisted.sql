b9d81644fc6a3e0baeeb903f8d1a509e00b65eee
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-01 11:52:51 -0800

    initial commit

---
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -0,0 +1,9 @@
+SELECT teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM teachers
+  JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY cohort,
+  teacher;
