d153764626ad2b8335b5a0e07e4b050cd05627ce
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-01 11:28:25 -0800

    refactor by pulling from assignments table rather than assistance_requests table

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,12 +1,9 @@
-SELECT assignment_id AS id,
-  assignments.name AS name,
-  assignments.day AS day,
-  assignments.chapter AS chapter,
-  COUNT(created_at) AS total_requests
-FROM assistance_requests
-  JOIN assignments ON assignments.id = assignment_id
-GROUP BY assignments.name,
-  assignments.day,
-  assignments.chapter,
-  assignment_id
+SELECT assignments.id,
+  name,
+  day,
+  chapter,
+  COUNT(assistance_requests) AS total_requests
+FROM assignments
+  JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
 ORDER BY total_requests DESC;
