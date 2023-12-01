fc4e6b0de6cae88bfee2be233084ee1befc0e44a
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-30 16:04:54 -0800

    add refactoring to match compass - FROM assignment_submissions rather than cohorts

--- 3_queries_group-by/4_total_assignment_submissions.sql
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -1,7 +1,7 @@
 SELECT cohorts.name as cohort,
   count(assignment_submissions) AS total_submissions
-FROM cohorts
-  JOIN students ON cohorts.id = cohort_id
-  JOIN assignment_submissions ON students.id = student_id
-GROUP BY cohort
+FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
 ORDER BY total_submissions DESC;
