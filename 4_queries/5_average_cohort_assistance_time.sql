cf0a836c8eb0735c7653dab206757be3d748d02b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-01 10:16:43 -0800

    confused created_at and started_at - NOW FIXED

--- 4_queries/5_average_cohort_assistance_time.sql
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -1,5 +1,5 @@
 SELECT cohorts.name,
-  AVG(completed_at - created_at) AS average_assistance_time
+  AVG(completed_at - started_at) AS average_assistance_time
 FROM assistance_requests
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = students.cohort_id
