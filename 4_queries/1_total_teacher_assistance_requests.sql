549122d7fa69845307b85662d7dab89cb52aa3fb
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-30 19:45:55 -0800

    refactor to match compass

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,6 +1,6 @@
-SELECT COUNT(teachers.id) AS total_assistances,
-  teachers.name AS name
-FROM teachers
-  JOIN assistance_requests ON teachers.id = teacher_id
-WHERE teachers.name = 'Waylon Boehm'
+SELECT count(assistance_requests.*) as total_assistances,
+  teachers.name
+FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
 GROUP BY teachers.name;
