253f57a677491886a1b30b3e45bbd2c5e9fbd398
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-01 11:03:05 -0800

    initial commit

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,10 @@
+SELECT avg(subquery_alias.total_duration) as average_total_duration
+FROM (
+    SELECT cohorts.name,
+      SUM(completed_at - started_at) AS total_duration
+    FROM assistance_requests
+      JOIN students ON students.id = student_id
+      JOIN cohorts ON cohorts.id = students.cohort_id
+    GROUP BY cohorts.name
+    ORDER BY total_duration
+  ) AS subquery_alias;
