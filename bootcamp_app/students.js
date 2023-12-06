258b4cff895140b78a3da4ea92817be4b6fa957a
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-05 17:53:31 -0800

    add query parameters & add pool.end

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2);
+
 pool.query(`
-SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-LIMIT 5;
-`)
+  SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
+  FROM students
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE '%${args[0]}%'
+  LIMIT ${args[1]};
+  `)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.student_name} has an ID of ${user.id} and was in the ${user.cohort_name} cohort!`)
     })
   })
-  .catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
